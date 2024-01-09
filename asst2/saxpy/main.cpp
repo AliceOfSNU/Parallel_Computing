c05851e2782ccfd62db5ec435b7d0a2cf45c276c
AliceOfSNU <khjune29@gmail.com>
2024-01-10 01:31:47 +0900

    gemm basic

--- asst2/saxpy/main.cpp
+++ asst2/saxpy/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 
 void saxpyCuda(int N, float alpha, float* x, float* y, float* result);
+void sgemm(int M, int N, int K, const float* A, const float* B, float* C);
 void printCudaInfo();
 
 
@@ -28,17 +29,22 @@ int main(int argc, char** argv)
 
     // parse commandline options ////////////////////////////////////////////
     int opt;
+    std::string task;
     static struct option long_options[] = {
         {"arraysize",  1, 0, 'n'},
         {"help",       0, 0, '?'},
+        {"task",    1,  0, 'm'},
         {0 ,0, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "?n:", long_options, NULL)) != EOF) {
+    while ((opt = getopt_long(argc, argv, "m:?n:", long_options, NULL)) != EOF) {
 
         switch (opt) {
         case 'n':
             N = atoi(optarg);
+            break;
+        case 'm':
+            task = optarg;
             break;
         case '?':
         default:
@@ -47,29 +53,79 @@ int main(int argc, char** argv)
         }
     }
     // end parsing of commandline options //////////////////////////////////////
+    if(task.compare("vec") == 0){
 
-    const float alpha = 2.0f;
-    float* xarray = new float[N];
-    float* yarray = new float[N];
-    float* resultarray = new float[N];
+        const float alpha = 2.0f;
+        float* xarray = new float[N];
+        float* yarray = new float[N];
+        float* resultarray = new float[N];
 
-    // load X, Y, store result
-    int totalBytes = sizeof(float) * 3 * N;
+        // load X, Y, store result
+        int totalBytes = sizeof(float) * 3 * N;
 
-    for (int i=0; i<N; i++) {
-        xarray[i] = yarray[i] = i % 10;
-        resultarray[i] = 0.f;
-   }
+        for (int i=0; i<N; i++) {
+            xarray[i] = yarray[i] = i % 10;
+            resultarray[i] = 0.f;
+        }
 
-    printCudaInfo();
+        printCudaInfo();
 
-    for (int i=0; i<3; i++) {
-      saxpyCuda(N, alpha, xarray, yarray, resultarray);
+        for (int i=0; i<3; i++) {
+        saxpyCuda(N, alpha, xarray, yarray, resultarray);
+        }
+
+        delete [] xarray;
+        delete [] yarray;
+        delete [] resultarray;
+
+    } else if (task.compare("gemm") == 0){
+        int N = 256, M = 256, K = 16;
+        float* A = new float[M * K];
+        float* B = new float[K * N];
+        float* C = new float[M * N];
+        float* C_ref = new float[M * N]; //cpu
+
+        //populate A
+        for(int y = 0; y < M; y += K){
+            for(int x = 0; x < K; ++x){
+                A[y + x] = x % 2;
+            }
+        }
+
+        //populate A
+        for(int y = 0; y < K; y += N){
+            for(int x = 0; x < N; ++x){
+                A[y + x] = x % 2;
+            }
+        }
+
+        //multiply
+        for(int y = 0; y < M; ++y){
+            for(int x = 0; x < N; ++x){
+                C_ref[y * N + x] = .0f;
+                for(int k = 0; k < K; ++k){
+                    C_ref[y * N + x] += A[y*K +k] * B[k*N + x];
+                }
+            }
+        }
+
+        //gpu impl
+        sgemm(M, N, K, A, B, C);
+
+        // check correctness
+        for(int y = 0; y < M; ++y){
+            for(int x = 0; x < N; ++x){
+                if(C[y * N + x] != C_ref[y * N + x]){
+                    printf("C[%d][%d] expected %f, but got %f \n", y, x, C_ref[y * N + x], C[y * N + x]);
+                    exit(1);
+                }
+            }
+        }
+
+        delete[] A;
+        delete[] B;
+        delete[] C;
+        delete[] C_ref;
     }
-
-    delete [] xarray;
-    delete [] yarray;
-    delete [] resultarray;
-
     return 0;
 }
--- asst2/saxpy/saxpy.cu
+++ asst2/saxpy/saxpy.cu
@@ -71,25 +71,81 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     cudaFree(device_x); cudaFree(device_y); cudaFree(device_result);
 }
 
+// MM kernels
+#define OUT
+#define CEIL_DIV(x, y) (((x)+(y)-1)/(y))
+#define BLOCK_SIZE 1024
 
 __global__ void
-gemm_kernel(int N, int M, int K, float* result) {
+gemm_kernel_naive(int M, int N, int K, 
+    const float* A, 
+    const float* B, 
+    OUT float* C
+) {
 
     // compute overall index from position of thread in current block,
     // and given the block we are in
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    __shared__ float sABlock[N][K];
-    __shared__ float sBBlock[K][M];
-    __shared__ float sCBlock[N][M];
+    //int tid = threadIdx.x+blockDim.x*(threadIdx.y+blockDim.y*threadIdx.z);
 
+    const uint tidx = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint tidy = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    if(tidx < M && tidy < N){
+        float tmp = 0.0;
+        for (int i = 0; i < K; ++i) {
+            tmp += A[tidx * K + i] * B[i * N + tidy];
+        }
+        C[tidx * N + tidy] = tmp;
+    }
     
 }
 
-#define OUT
+__global__ void
+gemm_kernel_coalesced(int M, int N, int K, 
+    const float* A, 
+    const float* B, 
+    OUT float* C
+) {
+
+    // compute overall index from position of thread in current block,
+    // and given the block we are in
+    //int tid = threadIdx.x+blockDim.x*(threadIdx.y+blockDim.y*threadIdx.z);
+
+    const uint tidx = blockIdx.x * BLOCK_SIZE + (threadIdx.x % BLOCK_SIZE);
+    const uint tidy = blockIdx.y * BLOCK_SIZE + (threadIdx.x / BLOCK_SIZE);
+    
+    if(tidy < M && tidx < N){
+        float tmp = 0.0;
+        for (int i = 0; i < K; ++i) {
+            tmp += A[tidy * K + i] * B[i * N + tidx];
+        }
+        C[tidy * N + tidx] = tmp;
+    }
+    
+}
+
+
+
+// hierarical GEMM
+// algorithm adapted from CuTe, Cutlass
+
+// this code runs in a single warp!
+// a warp always calculates outer product of 8*1 frag A and 1*4 frag B
+// and fills 8*4 block of C.
+// if tiling to larger spans, run outer loop which tiles into 8*4 sections, 
+// don't need syncs.
+__device__ void warp_gemm(int tid, int fragY, int fragX, float** sA, float** sB, OUT float** sC){
+    //fill in the answers~
+    int r = tid >> 0x08, c = tid & 0x08;
+    sC[r][c] += sA[r][0] * sB[0][c];
+}
+
 __device__ void
-block_gemm(int blockKDim, float** sABlock, float** sBBlock, OUT float** sCBlock, int threadYDim, int threadXDim){
-    float frag_a[threadYDim], frag_b[threadXDim];
-    float acc[threadYDim][threadXDim];
+block_gemm(int blockKDim, float** sABlock, float** sBBlock, OUT float** sCBlock, 
+    int threadYDim, int threadXDim)
+{
+    float frag_a[64*64], frag_b[64*64];
+    float acc[64*64];
     
     int blockX = 8, blockY = 4, blockTileK = 8;
 
@@ -115,15 +171,74 @@ block_gemm(int blockKDim, float** sABlock, float** sBBlock, OUT float** sCBlock,
 
 }
 
-// this code runs in a single warp!
-// a warp always calculates outer product of 8*1 frag A and 1*4 frag B
-// and fills 8*4 block of C.
-// if tiling to larger spans, run outer loop which tiles into 8*4 sections, 
-// don't need syncs.
-__device__ void warp_gemm(int tid, int fragY, int fragX, float** sA, float** sB, OUT float** sC){
-    //fill in the answers~
-    int r = tid >> 0x08, c = tid & 0x08;
-    sC[r][c] += sA[r][0] * sB[0][c];
+
+
+// calling code.
+# define ALGO ALGO_NAIVE
+# define ALGO_NAIVE 1
+# define ALGO_COAL 2
+void
+sgemm(int M, int N, int K, 
+    const float* A,
+    const float* B,
+    OUT float* C)
+{
+    // allocate memory on device.
+    float* device_A;
+    float* device_B;
+    float* device_C;
+    cudaMalloc(&device_A, M * K * sizeof(float));
+    cudaMalloc(&device_B, K * N * sizeof(float));
+    cudaMalloc(&device_C, M * N * sizeof(float));
+
+    // start timing after allocation of device memory
+    double startTime = CycleTimer::currentSeconds();
+
+    // copy input arrays to the GPU using cudaMemcpy
+    cudaMemcpy(device_A, A, M * K * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_B, B, K * N * sizeof(float), cudaMemcpyHostToDevice);
+
+    printf("%dx%d @ %dx%d multiplication\n", M, K, K, N);
+    // run kernel with timer
+
+
+    double kernelStartTime = CycleTimer::currentSeconds();
+
+    ////// IMPLEMENTATION HERE ///////
+#if ALGO == ALGO_NAIVE
+    //naive 39ms on 256*16 @ 16*256 task
+    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32), 1);
+    dim3 blockDim(32, 32, 1);
+    gemm_kernel_naive<<<gridDim, blockDim>>>(M, N, K, device_A, device_B, device_C);
+#elif ALGO == ALGO_COAL
+    //coalesced 26ms on 256*16 @ 16*256 task
+    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32), 1);
+    dim3 blockDim(BLOCK_SIZE); //1dim threads
+    gemm_kernel_coalesced<<<gridDim, blockDim>>>(M, N, K, device_A, device_B, OUT device_C);
+#endif
+    ////// END IMPL //////
+
+    cudaThreadSynchronize(); //must synchronize before timing
+
+    double kernelEndTime = CycleTimer::currentSeconds();
+    // copy result from GPU using cudaMemcpy
+    cudaMemcpy(C, device_C, M * N * sizeof(float), cudaMemcpyDeviceToHost);
+
+    double endTime = CycleTimer::currentSeconds();
+
+    cudaError_t errCode = cudaPeekAtLastError();
+    if (errCode != cudaSuccess) {
+        fprintf(stderr, "WARNING: A CUDA error occured: code=%d, %s\n", errCode, cudaGetErrorString(errCode));
+    }
+
+    double overallDuration = endTime - startTime;
+    printf("Overall: %.3f ms\n", 1000.f * overallDuration);
+
+    double kernelDuration = kernelEndTime - kernelStartTime;
+    printf("Kernel: %.3f ms\n", 1000.f*kernelDuration);
+
+    // free memory buffers on the GPU
+    cudaFree(device_A); cudaFree(device_B); cudaFree(device_C);
 }
 
 void
